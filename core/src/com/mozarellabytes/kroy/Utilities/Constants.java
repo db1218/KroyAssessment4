420f7f98d5a176a2c887f2fb68195c7bd8f4bb72
TogaTurtle <57758376+TogaTurtle@users.noreply.github.com>
2020-02-01 14:38:55 +0000

    Update Constants.java

--- core/src/com/mozarellabytes/kroy/Utilities/Constants.java
+++ core/src/com/mozarellabytes/kroy/Utilities/Constants.java
@@ -6,8 +6,8 @@ public final class Constants {
     public static final int GAME_WIDTH = 1024;
     public static final int GAME_HEIGHT = 720;
 
-    public static final int VIEWPORT_WIDTH = 40;
-    public static final int VIEWPORT_HEIGHT = 24;
+    public static final int VIEWPORT_WIDTH = 48;
+    public static final int VIEWPORT_HEIGHT = 29;
 
     public static final float TILE_WxH = 48f; //Tile width and height in pixels
 
